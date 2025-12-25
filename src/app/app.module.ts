2dc2724982d286d295e27f78a213ee27c9a67d7a
Maurici0M. <mauriciomarquescosta7@gmail.com>
2025-12-25 20:14:28 -0300

    site organization adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './library/card/card.component';
+import { CarouselComponent } from './library/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
