39b312b6506cff573daadc869ca8a33780ff0ccc
Maurici0M. <mauriciomarquescosta7@gmail.com>
2025-12-28 12:13:14 -0300

    Adding a functional structure from Swiper.js to the project in a reusable way for cards.

--- src/app/library/carousel/carousel.component.ts
+++ src/app/library/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CardPrimary } from '../card/interface';
 
 @Component({
   selector: 'app-carousel',
@@ -6,6 +7,64 @@ import { Component } from '@angular/core';
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
+
 export class CarouselComponent {
 
+  swiperBreakPoints = {
+    0: { slidesPerView: 1, spaceBetween: 0 },
+    660: { slidesPerView: 2, spaceBetween: 30 },
+    950: { slidesPerView: 3, spaceBetween: 30 },
+    1300: {slidesPerView: 4, spaceBetween: 30}
+  }
+
+  dataCard: CardPrimary[] = [
+      {
+        title: "Discord: O que é e como funciona o app para gamers que virou foco de abusos contra jovens",
+        description: "Plataforma ganhou popularidade entre os jovens por transmissões ao vivo de vídeos das partidas, mas virou mecanismo da ação de criminosos para subornar menores",
+        srcImg: "https://4kwallpapers.com/images/walls/thumbs/24799.jpg"
+      },
+
+      {
+        title: "Card 2",
+        description: "",
+        srcImg: "https://4kwallpapers.com/images/walls/thumbs/1684.jpg"
+      },
+
+      {
+        title: "Card 3",
+        description: "Descricao 3",
+        srcImg: "https://4kwallpapers.com/images/walls/thumbs/5630.jpg"
+      },
+
+      {
+        title: "Card 4",
+        description: "Descricao 4",
+        srcImg: "https://imgs.search.brave.com/3IhricSR8EHQ3KQxLHXGsXdVcqXkzQpTtgfZz34oMJw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzIxLzE4LzAz/LzM2MF9GXzUyMTE4/MDM4NF84MzdaUVZ0/R2RCb2NuS1FyNXBy/eFd6cGpJYTY0d3Rn/Ty5qcGc"
+      },
+
+      {
+        title: "Discord: O que é e como funciona o app para gamers que virou foco de abusos contra jovens",
+        description: "Plataforma ganhou popularidade entre os jovens por transmissões ao vivo de vídeos das partidas, mas virou mecanismo da ação de criminosos para subornar menores",
+        srcImg: "https://4kwallpapers.com/images/walls/thumbs/24799.jpg"
+      },
+
+      {
+        title: "Card 6",
+        description: "Descricao 6",
+        srcImg: "https://4kwallpapers.com/images/walls/thumbs/1684.jpg"
+      },
+
+      {
+        title: "Card 7",
+        description: "Descricao 7",
+        srcImg: "https://4kwallpapers.com/images/walls/thumbs/5630.jpg"
+      },
+
+      {
+        title: "Card 8",
+        description: "Descricao 8",
+        srcImg: "https://imgs.search.brave.com/3IhricSR8EHQ3KQxLHXGsXdVcqXkzQpTtgfZz34oMJw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzIxLzE4LzAz/LzM2MF9GXzUyMTE4/MDM4NF84MzdaUVZ0/R2RCb2NuS1FyNXBy/eFd6cGpJYTY0d3Rn/Ty5qcGc"
+      },
+
+  ]
 }
