398d4de64972c6f6b90873016c310956f8ddd5ef
Maurici0M. <mauriciomarquescosta7@gmail.com>
2025-12-28 12:01:57 -0300

    Import the Swiper.js module and register it for use in the project.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
+
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
 })
