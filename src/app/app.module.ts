790d8b1e99a8f9b419fb20e5140040a22bb6d51c
Maurici0M. <mauriciomarquescosta7@gmail.com>
2025-12-31 17:11:07 -0300

    imports of new components and dependencies in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import {MatIconModule} from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './library/card/card.component';
 import { CarouselComponent } from './library/carousel/carousel.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { TechnologyComponent } from './components/pages/technology/technology.component';
+import { SidenavComponent } from './library/sidenav/sidenav.component';
+import { ContainerPagesComponent } from './components/container-pages/container-pages.component';
+import { CulinaryComponent } from './components/pages/culinary/culinary.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    CarouselComponent
+    CarouselComponent,
+    HomeComponent,
+    TechnologyComponent,
+    SidenavComponent,
+    ContainerPagesComponent,
+    CulinaryComponent,
+    AboutUsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
