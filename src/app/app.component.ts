1131041530d875cb9c66b3f087c65cee548cec8a
Maurici0M. <mauriciomarquescosta7@gmail.com>
2025-12-31 15:45:59 -0300

    adjustments to start the containerization of components in the application and addition of logic for rendering pages dynamically using input and output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CardPrimary } from './library/card/interface';
+import { TypePage } from './components/container-pages/i-type-page';
 
 @Component({
   selector: 'app-root',
@@ -8,29 +8,18 @@ import { CardPrimary } from './library/card/interface';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ang-carrossel-de-musicas';
 
-  dataCard: CardPrimary[] = [
-    {
-      title: "Discord: O que é e como funciona o app para gamers que virou foco de abusos contra jovens",
-      description: "Plataforma ganhou popularidade entre os jovens por transmissões ao vivo de vídeos das partidas, mas virou mecanismo da ação de criminosos para subornar menores",
-      srcImg: "https://4kwallpapers.com/images/walls/thumbs/24799.jpg"
-    },
-    {
-      title: "Card 2",
-      description: "Descricao 2",
-      srcImg: "https://4kwallpapers.com/images/walls/thumbs/1684.jpg"
-    },
-    {
-      title: "Card 3",
-      description: "Descricao 3",
-      srcImg: "https://4kwallpapers.com/images/walls/thumbs/5630.jpg"
-    },
-    {
-      title: "Card 4",
-      description: "Descricao 4",
-      srcImg: "https://imgs.search.brave.com/3IhricSR8EHQ3KQxLHXGsXdVcqXkzQpTtgfZz34oMJw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA1LzIxLzE4LzAz/LzM2MF9GXzUyMTE4/MDM4NF84MzdaUVZ0/R2RCb2NuS1FyNXBy/eFd6cGpJYTY0d3Rn/Ty5qcGc"
-    },
+  currentPage: TypePage = {
+    current: 'home'
+  };
 
-  ]
+  constructor() {
+  }
+
+  handleChangePage(event: any) {
+    this.currentPage = {
+      current: event
+    }
+  }
+
 }
