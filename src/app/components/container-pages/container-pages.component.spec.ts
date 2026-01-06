a1c0f7065010b2aa5958884c654d960770a118c7
Maurici0M. <mauriciomarquescosta7@gmail.com>
2026-01-06 00:24:03 -0300

    Adding unit tests to the project to cover all component functions.

--- src/app/components/container-pages/container-pages.component.spec.ts
+++ src/app/components/container-pages/container-pages.component.spec.ts
@@ -1,23 +1,89 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContainerPagesComponent } from './container-pages.component';
+import { TypePage } from './i-type-page';
 
 describe('ContainerPagesComponent', () => {
   let component: ContainerPagesComponent;
-  let fixture: ComponentFixture<ContainerPagesComponent>;
+
+  let spyHandleChangePage: jasmine.Spy;
+
+  let spyOutputChangePage: jasmine.Spy;
+
+  let spyNgOnInit: jasmine.Spy;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ContainerPagesComponent]
-    })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(ContainerPagesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new ContainerPagesComponent();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngOnInit', () => {
+
+    beforeEach(() => {
+      spyNgOnInit = spyOn(component, 'ngOnInit').and.callThrough();
+    });
+
+    it('Verificando se a funcao foi chamada uma vez', () => {
+      component.ngOnInit();
+
+      expect(spyNgOnInit).toHaveBeenCalledTimes(1);
+    });
   });
+
+  describe('handleChangePage', () => {
+    let dataPage: TypePage = {
+      current: 'home'
+    };
+
+    beforeEach(() => {
+      spyHandleChangePage = spyOn(component, 'handleChangePage').and.callThrough();
+
+      spyOutputChangePage = spyOn(component.changePage, 'emit').and.callThrough();
+    });
+
+    it('Verificando se a funcao foi chamada uma vez', () => {
+      let fake: TypePage = {
+        current: 'home'
+      }
+
+      component.handleChangePage(dataPage);
+
+      expect(spyHandleChangePage).toHaveBeenCalledTimes(1);
+
+    });
+
+    it('Verificando se a funcao foi chamada emitindo os parametros corretos', () => {
+
+      // --- arrange ---
+
+      // cria uma variavel 'clone' do dataPage somente para validar os resultados no assert
+      let fakeData: TypePage = {
+        current: 'home'
+      }
+
+      // --- act ---
+
+      // chama a funcao uma vez durante o teste com os parametros da variavel dataPage
+      component.handleChangePage(dataPage);
+
+      // --- assert ---
+
+      // verificando se a funcao foi chamada uma vez
+      expect(spyHandleChangePage).toHaveBeenCalledTimes(1);
+
+      // verificando se a funcao foi chamada com os parametros corretos sem erro
+      expect(spyHandleChangePage).toHaveBeenCalledWith(dataPage);
+
+      // verificando se o evento output foi emitido uma vez
+      expect(component.changePage.emit).toHaveBeenCalledTimes(1);
+
+      // verificando se o evento output foi emitido com os parametros corretos
+
+      /*
+        Aqui foi verificado se o evento output foi emitido com o fakeData (que são os mesmos dados da variável dataPage)
+        somente para garantir que os dados são iguais
+      */
+      expect(component.changePage.emit).toHaveBeenCalledWith(fakeData);
+    });
+
+  });
+
 });
--- src/app/components/pages/home/home.component.spec.ts
+++ src/app/components/pages/home/home.component.spec.ts
@@ -1,23 +1,90 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { CarouselService, PageCard } from '../../../services/carousel-service.service';
+import { UtilsService } from '../../../shared/functions/utils.service';
+import { of } from 'rxjs';
+import { MOCK_HOME } from '../../../mocks/carousel';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [HomeComponent]
-    })
-    .compileComponents();
+  let carouselService: jasmine.SpyObj<CarouselService>;
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  let functionsUtils = new UtilsService();
+
+  let spyNgOnInit: jasmine.Spy;
+
+  let spyGetCardsCarousel: jasmine.Spy;
+
+  beforeEach(() => {
+    carouselService = jasmine.createSpyObj('CarouselService', ['getCards']);
+
+    /*
+      Quando o servico for chamado no onInit, ele retornara um mock de dados para o carrossel
+      (Necessario para evitar o erro nos testes com o subscribe);
+    */
+
+    carouselService.getCards.and.returnValue(of(MOCK_HOME));
+
+    component = new HomeComponent(
+      carouselService,
+      functionsUtils
+    );
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('ngOnInit', () => {
+    beforeEach(() => {
+      spyNgOnInit = spyOn(component, 'ngOnInit').and.callThrough();
+
+      spyGetCardsCarousel = spyOn(component, 'getCardsCarousel').and.callThrough();
+    });
+
+    it('verificando se a funcao foi chamada uma vez', () => {
+      // --- act
+      component.ngOnInit();
+
+      // --- assert
+      expect(spyNgOnInit).toHaveBeenCalledTimes(1);
+
+      expect(spyGetCardsCarousel).toHaveBeenCalledTimes(1);
+    });
+
+  });
+
+  describe('getCardsCarousel', () => {
+    let pageName: PageCard = {
+      name: 'home'
+    };
+
+    beforeEach(() => {
+      spyOn(functionsUtils, 'getPageCard').and.callThrough();
+    });
+
+    it('Verificando se o servico é chamado corretamente e armazena os dados na variavel dataCard para criar o array de cards', () => {
+      // --- act
+      carouselService.getCards.and.returnValue(of(MOCK_HOME));
+
+      component.getCardsCarousel();
+
+      // --- assert
+
+      // validacoes da funcao que gera o pageName
+      expect(functionsUtils.getPageCard).toHaveBeenCalledTimes(1);
+
+      expect(functionsUtils.getPageCard).toHaveBeenCalledWith(pageName.name);
+
+      // validacoes do servico
+      expect(carouselService.getCards).toHaveBeenCalledTimes(1);
+
+      expect(carouselService.getCards).toHaveBeenCalledWith(pageName);
+
+      expect(component.dataCard).toBe(MOCK_HOME.card);
+    });
+
+  });
+
 });
