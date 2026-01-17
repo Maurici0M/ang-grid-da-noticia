2c84222423b0e72813495e66ed107e445b1a6ded
Maurici0M. <mauriciomarquescosta7@gmail.com>
2026-01-17 00:06:16 -0300

    Creating a typing interface for footer elements.

---
+++ src/app/interfaces/footer/i-footer.ts
@@ -0,0 +1,23 @@
+export interface FooterItem {
+  text: string,
+  route?: string
+}
+
+export interface FooterSocialMedia {
+  socialMediaIcon: string,
+  linkSocialMedia: string
+}
+
+export interface FooterItemSocialMedia extends FooterItem {
+  social: FooterSocialMedia[]
+}
+
+export interface DataFooter {
+  seeAlso: FooterItem[],
+
+  connect: FooterItem[],
+
+  learnMore: FooterItem[],
+
+  followUs: FooterItemSocialMedia[]
+}
